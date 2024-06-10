21c37e31b10cf2af51285b2a9a91b0578cc6e937
Md <mdakram12022002@gmail.com>
2024-06-10 22:14:48 +0530

    implimentaion of the Hoisting with Function in

--- lecture19/classHoisting.js
+++ lecture19/classHoisting.js
@@ -1,22 +1,47 @@
 
 
-function solve(number) {
-    return function (number) {
-        return number * number;
-    }
-}
-let ans = solve();
-let finalAns = ans(10);
-console.log(finalAns);
+// function solve(number) {
+//     return function (number) {
+//         return number * number;
+//     }
+// }
+// let ans = solve();
+// let finalAns = ans(10);
+// console.log(finalAns);
+
+const { Container } = require("postcss")
 
 
 
 
+// const arr = [
+//     function(a, b) {
+//         return a + b;
+//     },
+//     function(a, b) {
+//         return a - b;
+//     },
+//     function(a, b) {
+//         return a * b;
+//     }
+// ];
+// let ans = arr[2];
+// let finalAns = ans(10, 10);
+// console.log(finalAns);
 
 
 
 
-
+// let obj = {
+//     name: "akram",
+//     age: 22,
+//     wet: 50,
+//     het: 5.7,
+//     greet(){
+//         console.log("Hello je kese hain aap")
+//     }
+// }
+// console.log(obj.greet);
 
 
 
@@ -36,3 +61,19 @@ console.log(finalAns);
 // }
 // greetMe(greet, "Akram");
 // greet();
+
+// greet(); // function are not hoisting but variable is hoisting if we want, with the keyWord using 'var'.
+// console.log(greet); // this is also not hoisting becouse of we initializing keyword is let that's why it give the ReferenceError.
+
+// console.log(greet); // give the output 'undefined'
+// let greet = function(){
+//     console.log("Hello World!")
+// }
+
+// how we hoisting the function usig the keyWord 'var' --->
+var greet;
+console.log(greet); // give the output 'undefined'
+
+var greet = function () {
+    console.log("Hello World!")
+}
