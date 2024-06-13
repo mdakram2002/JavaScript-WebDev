db47c71902b04147705dbfd20969d37bbcf872be
Md <mdakram12022002@gmail.com>
2024-06-13 12:26:13 +0530

    implimentaion of In-Built Object

---
+++ lecture22/date.js
@@ -0,0 +1,5 @@
+let date = new Date();
+
+let newDate = new Date('16, December, 2001, 07:00 PM');
+
+console.log(newDate);
---
+++ lecture22/inBuiltObject.js
@@ -0,0 +1,19 @@
+// In-built Object-->
+
+// console.log(Math.PI);
+
+// console.log(Math.max(66,77,88,65,9,776,3,3323,3));
+// console.log(Math.min(66,77,88,65,9,776,3,3323,3));
+
+// console.log(Math.round(1.7)); // 1.5 to just big num
+// console.log(Math.round(1.4)); // 1.5 to just small num
+
+// console.log(Math.ceil(1.2));  // 1 to just big num.
+// console.log(Math.floor(1.9)); // floor that mean lower value
+
+// console.log(Math.abs(-6));    // abs  means absolute value or the give the positive output
+// console.log(Math.random());   // give the output randome value
+
+// console.log(Math.sqrt(5));     // squar of the number
+// console.log(Math.pow(2,10));   // power of the num
+
