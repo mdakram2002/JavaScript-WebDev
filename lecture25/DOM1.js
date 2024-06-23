91ab6946dd97cb7d8d0d3aa87fc5d6886b57f960
Md <mdakram12022002@gmail.com>
2024-06-23 15:48:35 +0530

    Implimentaion of DOM

--- lecture25/DOM1.js
+++ lecture25/DOM1.js
@@ -8,9 +8,18 @@
 
 // console.log(container.firstElementChild.nodeName);
 
-let fdiv = document.querySelector('#fdiv');
+// let fdiv = document.querySelector('#fdiv');
 
-let newElement = document.createElement('span')
-newElement.textContent = "Hay, this is Akram Shaikh";
+// let newElement = document.createElement('span')
+// newElement.textContent = "Hay, this is Akram Shaikh";
 
-fdiv.insertAdjacentElement('beforebegin', newElement);
+// fdiv.insertAdjacentElement('beforebegin', newElement);
+
+// let parent = document.querySelector('#mydiv');
+// let child = document.querySelector('#fpara');
+// parent.removeChild(child);
+
+let secondElement = doucment.getElementById('mydiv');
+console.log(secondElement);
+
+secondElement.style.cssText;
