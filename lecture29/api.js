61813c9f12beb949290969f1960fda8ba04d8759
Md <mdakram12022002@gmail.com>
2024-06-29 18:04:39 +0530

    Learn how APIs work & its implimentaions

--- lecture29/api.js
+++ lecture29/api.js
@@ -1,2 +1,29 @@
-console.log("WELCOME TO API session");
-const URL = "https://cat-fact.herokuapp.com";
+
+// Implimentation of APIs
+const URL = "https://cat-fact.herokuapp.com/facts";
+const factsPara = document.querySelector("#facts");
+const btn = document.querySelector("#btn");
+
+// const getFacts = async () => {
+//     let response = await fetch(URL);
+//     console.log(response); // JSON format or AJAJ
+
+//     let data = await response.json();
+//     factsPara.inneraText = data[3].text;
+//     console.log(data[3]);
+// };
+
+function getFacts() {
+    fetch(URL).then((response) => {
+        return response.json();
+    })
+        .then((data) => {
+            console.log(data);
+            factsPara.innerText = data[2].text;
+            console.log(data[2])
+        });
+};
+
+btn.addEventListener("click", getFacts);
+
+
